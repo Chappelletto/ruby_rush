b1f28ad28b9d77ead2436cc4000ca9a5909140da
Chinaski <sirchappelletto@rambler.ru>
2023-02-26 13:10:17 +0400

    step 25 complete

---
+++ step_23_array.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+#массив мужчин
+men = ["Gena", "George", "Misha"]
+
+#массив женщин
+women = ["Kate", "Liza", "Masha"]
+
+all = men + women
+
+puts men.to_s
+puts women.to_s
+
+puts "-------"
+
+puts all.to_s
---
+++ step_24_invert_array.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+array = [1, 2, 3, 4, 5]
+
+puts array.to_s
+
+puts array.reverse.to_s
+
+puts array.to_s
+
+puts array.reverse!.to_s
+
+puts array.to_s
